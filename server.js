94bfdd4484d02becece91ffd33ce1b87682ff814
Moshik Alper <moshikalper@gmail.com>
2024-10-28 16:18:53 +0200

    add bugs json

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import { utilService } from './services/util.service.js'
+
+const bugs = utilService.readJsonFile('data/bugs.json')
+console.log('bugs:', bugs)
 
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
+app.get('/api/bug', (req, res) => {
+    res.send(bugs)
+})
+
+
 app.listen(3030, () => console.log('Server ready at port 3030'))
