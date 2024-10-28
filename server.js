cebb70284cbabebdea55a24ac5cdb428d4344925
Moshik Alper <moshikalper@gmail.com>
2024-10-28 18:45:48 +0200

    Add remove and getById

--- server.js
+++ server.js
@@ -1,13 +1,64 @@
 import express from 'express'
-import { utilService } from './services/util.service.js'
-
-const bugs = utilService.readJsonFile('data/bugs.json')
-console.log('bugs:', bugs)
+import { bugService } from './services/bug.service.js'
 
 const app = express()
+
+
 app.get('/api/bug', (req, res) => {
-    res.send(bugs)
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            console.log('Cannot get bugs:', err)
+            res.status(500).send('Cannot get bugs')
+        })
 })
 
 
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.get('/api/bug/:bugId', (req, res) => {
+    console.log('req.params:', req.params)
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            console.log('Cannot get bug:', err)
+            res.status(500).send('Cannot get bug')
+        })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(bugId + ' Removed Successfully!'))
+        .catch(err => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug')
+        })
+})
+
+// app.get('/api/bug/save', (req, res) => {})
+
+// import EventEmitter from 'node:events'
+
+// const eventBus = new EventEmitter()
+// eventBus.on('say_tal', (data) => {
+//     console.log('started', data)
+
+//   })
+
+// setTimeout(() => {
+//     eventBus.emit('say_tal', ['12345'])
+// }, 2000)
+
+app.use((req, res, next) => {
+    // console.log('Time:', Date.now())
+    console.log('req:', req.host)
+    console.log('req:', req.hostname)
+    console.log('req:', req.headers)
+    next()
+})
+
+
+const port = 3030
+app.listen(port, () =>
+    console.log(`Server listening on port http://127.0.0.1:${port}/`)
+)
---
+++ services/bug.service.js
@@ -0,0 +1,87 @@
+
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+import EventEmitter from 'node:events'
+import fs from 'fs'
+
+// const STORAGE_KEY = 'bugDB'
+// _createBugs()
+const bugs = utilService.readJsonFile('data/bugs.json')
+
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+}
+
+// const eventBus = new EventEmitter()
+// setTimeout(() => {
+//     eventBus.emit('say_tal', ['12345'])
+// }, 2000)
+
+function query() {
+    return Promise.resolve(bugs)
+    //...later
+}
+
+function getById(bugId) {
+    const bug = bugs.find(bug => bug._id === bugId)
+    if (!bug) return Promise.reject('Cannot find bug - ' + bugId)
+        return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+    const bugIdx = bugs.findIndex(bug => bug._id === bugId)
+    if (bugIdx < 0) return Promise.reject('Cannot find bug - ' + bugId)
+    bugs.splice(bugIdx, 1)
+    return _saveBugsToFile()
+}
+
+function _saveBugsToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('data/bugs.json', data, (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
+
+function save(bug) {
+
+}
+
+function _createBugs() {
+    let bugs = utilService.loadFromStorage(STORAGE_KEY)
+    if (!bugs || !bugs.length) {
+        bugs = [
+            {
+                title: "Infinite Loop Detected",
+                severity: 4,
+                _id: "1NF1N1T3"
+            },
+            {
+                title: "Keyboard Not Found",
+                severity: 3,
+                _id: "K3YB0RD"
+            },
+            {
+                title: "404 Coffee Not Found",
+                severity: 2,
+                _id: "C0FF33"
+            },
+            {
+                title: "Unexpected Response",
+                severity: 1,
+                _id: "G0053"
+            }
+        ]
+        utilService.saveToStorage(STORAGE_KEY, bugs)
+    }
+
+
+
+}
