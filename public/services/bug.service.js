c8338273aab37279625750d30c75cb14f465d403
Moshik Alper <moshikalper@gmail.com>
2024-10-29 15:53:16 +0200

    Refactor the bugService to use API instead of using localStorage

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -1,10 +1,6 @@
 
-import { storageService } from './async-storage.service.js'
-import { utilService } from './util.service.js'
+const BASE_URL = '/api/bug/'
 
-const STORAGE_KEY = 'bugDB'
-
-_createBugs()
 
 export const bugService = {
     query,
@@ -13,57 +9,28 @@ export const bugService = {
     remove,
 }
 
-
 function query() {
-    return storageService.query(STORAGE_KEY)
+    return axios.get(BASE_URL)
+        .then(res=>res.data)
 }
+
 function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
+    return axios.get(BASE_URL + bugId)
+        .then(res=>res.data)
 }
 
 function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+    return axios.get(BASE_URL + bugId + '/remove')
+        .then(res=>res.data)
 }
 
 function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
-}
-
-
-
-
-function _createBugs() {
-    let bugs = utilService.loadFromStorage(STORAGE_KEY)
-    if (!bugs || !bugs.length) {
-        bugs = [
-            {
-                title: "Infinite Loop Detected",
-                severity: 4,
-                _id: "1NF1N1T3"
-            },
-            {
-                title: "Keyboard Not Found",
-                severity: 3,
-                _id: "K3YB0RD"
-            },
-            {
-                title: "404 Coffee Not Found",
-                severity: 2,
-                _id: "C0FF33"
-            },
-            {
-                title: "Unexpected Response",
-                severity: 1,
-                _id: "G0053"
-            }
-        ]
-        utilService.saveToStorage(STORAGE_KEY, bugs)
-    }
-
-
-
+  const url = BASE_URL + 'save'
+  let queryParams = `?title=${bug.title}&severity=${bug.severity}&description=${bug.description}`
+  if (bug._id) queryParams += `&_id=${bug._id}`
+  return axios.get(url + queryParams)
+      .then(res => res.data)
+      .catch(err => {
+        console.log('err:', err)
+      })
 }
