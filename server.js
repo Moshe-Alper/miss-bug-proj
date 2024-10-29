c20ac44aa35e939bbf02b49688b5b88ea40450d2
Moshik Alper <moshikalper@gmail.com>
2024-10-29 14:47:29 +0200

    Add frontend code

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 import express from 'express'
-import EventEmitter from 'node:events'
-
 import { bugService } from './services/bug.service.js'
-
+import { loggerService } from './services/logger.service.js'
+import EventEmitter from 'node:events'
 const app = express()
 
-
+app.use(express.static('public'))
 
 
 app.get('/api/bug', (req, res) => {
     bugService.query()
         .then(bugs => res.send(bugs))
         .catch(err => {
-            console.log('Cannot get bugs:', err)
+            loggerService.error('Cannot get bugs:', err)
             res.status(500).send('Cannot get bugs')
         })
 })
@@ -28,7 +27,7 @@ app.get('/api/bug/save', (req, res) => {
     bugService.save(bugToSave)
     .then(savedBug => res.send(savedBug))
     .catch(err => {
-        console.log('Cannot save bug:', err)
+        loggerService.error('Cannot save bug:', err)
         res.status(500).send('Cannot save bug')
     })
 })
@@ -38,7 +37,7 @@ app.get('/api/bug/:bugId', (req, res) => {
     bugService.getById(bugId)
         .then(bug => res.send(bug))
         .catch(err => {
-            console.log('Cannot get bug:', err)
+            loggerService.error('Cannot get bug:', err)
             res.status(500).send('Cannot get bug')
         })
 })
@@ -53,6 +52,11 @@ app.get('/api/bug/:bugId/remove', (req, res) => {
         })
 })
 
+// Log in browser (temporary - will not be used later)
+app.get('/api/logs', (req, res) => {
+    const path = process.cwd()
+    res.sendFile(path + '/logs/backend.log')
+})
 
 // const eventBus = new EventEmitter()
 // eventBus.on('say_hello', (data) => {
@@ -75,5 +79,5 @@ app.get('/api/bug/:bugId/remove', (req, res) => {
 
 const port = 3030
 app.listen(port, () =>
-    console.log(`Server listening on port http://127.0.0.1:${port}/`)
+    loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
 )
