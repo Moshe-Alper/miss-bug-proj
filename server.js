8140331d567ec64736d4f163b57837a348a3bcbe
Moshik Alper <moshikalper@gmail.com>
2024-11-11 15:26:20 +0200

    Add userRoute (backend)

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import path from 'path'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import EventEmitter from 'node:events'
+
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
+import { userService } from './services/user.service.js'
 import { pdfService } from './services/pdf.service.js'
+
+
 
 const app = express()
 
@@ -72,6 +76,27 @@ app.delete('/api/bug/:bugId', (req, res) => {
         .catch(err => {
             loggerService.error('Cannot remove bug', err)
             res.status(500).send('Cannot remove bug')
+        })
+})
+
+// User API
+app.get('/api/user', (req, res) => {
+    userService.query()
+        .then(users => res.send(users))
+        .catch(err => {
+            loggerService.error('Cannot load users', err)
+            res.status(400).send('Cannot load users')
+        })
+})
+
+app.get('/api/user/:userId', (req, res) => {
+    const { userId } = req.params
+
+    userService.getById(userId)
+        .then(user => res.send(user))
+        .catch(err => {
+            loggerService.error('Cannot load user', err)
+            res.status(400).send('Cannot load user')
         })
 })
 
--- services/user.service.js
+++ services/user.service.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import Cryptr from 'cryptr'
 
 import { utilService } from './util.service.js'
-
 const cryptr = new Cryptr(process.env.SECRET1 || 'secret-puk-1234')
 const users = utilService.readJsonFile('data/user.json')
 
